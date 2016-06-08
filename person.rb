4a690e3e4d7978522925114cad2dc94d1222d731
Carrie Liang <20.c.liang@gmail.com>
2016-06-07 23:09:10 -0700

    use constants for TARGET RAISE and RATING, move salary and targetraise attr to staff member class

--- person.rb
+++ person.rb
@@ -1,10 +1,26 @@
+require_relative 'highfiveable'
+
 class Person
   attr_reader :age, :phase
   attr_accessor :name
 
-  def initialize(options = {})
-    @phase = 1
+  include HighFiveable
+
+  def initialize(options={})
+    @phase = 3
     @age = options.fetch(:age, 0)
     @name = options.fetch(:name, "")
+  end
+
+  def set_phase(num)
+    response = ""
+    if num == @phase
+      response += "I'm doing phase #{@phase} again because "
+      response += "I put my learning first. I'm gonna rock it!"
+    else
+      response = "Oooh, phase #{num}. I hope I'm ready!"
+    end
+    @phase = num
+    response
   end
 end
--- student.rb
+++ student.rb
@@ -1,19 +1,9 @@
+require_relative 'person'
+
 class Student < Person
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    response = ""
-    if num == @phase
-      response += "I'm doing phase #{@phase} again because "
-      response += "I put my learning first. I'm gonna rock it!"
-    else
-      response = "Oooh, phase #{num}. I hope I'm ready!"
-    end
-    @phase = num
-    response
+  def initialize(options = {})
+    super
+    @phase = 1
   end
 
   def learn_stuff
